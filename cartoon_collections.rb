f894ff16bdd7db10a97ed2cb1be6536d25d671c7
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-18 20:35:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ def long_planeteer_calls(arr)# code an argument here
   return arr.any?{|calls| calls.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.each do 
 end
